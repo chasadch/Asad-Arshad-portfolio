b8f7ce2c166318a98f647670582c79c0d47307ee
chasadch <arshadasad566@gmail.com>
2025-09-17 02:04:38 +0500

    change pic

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
+    domains: ['localhost'],
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
   },
